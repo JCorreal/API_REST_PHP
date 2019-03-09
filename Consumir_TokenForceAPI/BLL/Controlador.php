d4567ef9cfcbde048f015dc946b4b9976c2f7856
JCorreal <37590391+JCorreal@users.noreply.github.com>
2019-03-09 08:16:28 -0500

    Add files via upload

--- Consumir_TokenForceAPI/BLL/Controlador.php
+++ Consumir_TokenForceAPI/BLL/Controlador.php
@@ -1,8 +1,6 @@
 <?php
-     require_once '../BO/Usuario.php';
-     
-     
- 
+     require_once '../BO/Usuario.php';     
+      
      if ($_POST['itCampoClave'] == 'C')
      {        
         $usuario = new Usuario();
@@ -27,52 +25,41 @@
             echo 'Error. Servicio no pudo ser ejecutado';
         }
      }
-      elseif ($_POST['itCampoClave'] == 'R') 
+     else 
      {
-       $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id');  
-       $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar; 
-       
-        $ch = curl_init($url);
-  	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
-        $response = curl_exec($ch);
-        curl_close($ch);  
-        echo $response;
-     }             
-     elseif ($_POST['itCampoClave'] == 'U') 
-     {  
-         $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id'); 
-         $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar;
-         $usuario = new Usuario();
-         $usuario->setUsuario_id(filter_input(INPUT_POST, 'itUsuario_id',FILTER_SANITIZE_NUMBER_INT));
-         $usuario->setNombres(filter_input(INPUT_POST, 'itNombres',FILTER_SANITIZE_STRING));
-         $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));       
+        if ($_POST['itCampoClave'] == 'R') 
+        {
+         $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id');  
+         $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar; 
          $ch = curl_init($url);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($usuario));
-      
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
-        $response = curl_exec($ch);
-        curl_close($ch);  
-        echo $response;
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");           
+       }             
+       elseif ($_POST['itCampoClave'] == 'U') 
+       {  
+           $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id'); 
+           $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar;
+           $usuario = new Usuario();
+           $usuario->setUsuario_id(filter_input(INPUT_POST, 'itUsuario_id',FILTER_SANITIZE_NUMBER_INT));
+           $usuario->setNombres(filter_input(INPUT_POST, 'itNombres',FILTER_SANITIZE_STRING));
+           $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));       
+           $ch = curl_init($url);
+           curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($usuario));
+           curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+       }
+       elseif ($_POST['itCampoClave'] == 'D') 
+       {
+         $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id');  
+         $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar ; 
+         $ch = curl_init($url);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");       
+       }     
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
+          $response = curl_exec($ch);
+          curl_close($ch);  
+          echo $response;
+     
      }
-     elseif ($_POST['itCampoClave'] == 'D') 
-     {
-       $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id');  
-       $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar ; 
-       
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");        
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
-        $response = curl_exec($ch);
-        curl_close($ch);  
-        echo $response;
-     }     
-     
-     
       
 
 
