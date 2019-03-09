22bed64325924511b92d24edc47f6685729fd988
JCorreal <37590391+JCorreal@users.noreply.github.com>
2019-03-09 08:53:18 -0500

    Update Controlador.php

--- Consumir_TokenForceAPI/BLL/Controlador.php
+++ Consumir_TokenForceAPI/BLL/Controlador.php
@@ -6,61 +6,43 @@
         $usuario = new Usuario();
         $usuario->setUsuario_id(0);
         $usuario->setNombres(filter_input(INPUT_POST, 'itNombres',FILTER_SANITIZE_STRING));
-        $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));
+        $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));     
         
         $url = 'http://localhost:83/TokenForceAPI/usuarios'; 
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($usuario));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                
-        $response = json_decode(curl_exec($ch), true);                         
-        curl_close($ch);  
-
-        if ($response['status'] == 'Exito')  
-        {
-            echo 'Servicio consumido con exito y datos fueron grabados';
-        }
-        else 
-        {         
-            echo 'Error. Servicio no pudo ser ejecutado';
-        }
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");        
      }
-     else 
+     elseif ($_POST['itCampoClave'] == 'R') 
      {
-        if ($_POST['itCampoClave'] == 'R') 
-        {
          $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id');  
          $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar; 
          $ch = curl_init($url);
          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");           
-       }             
-       elseif ($_POST['itCampoClave'] == 'U') 
-       {  
-           $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id'); 
-           $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar;
+     }             
+     elseif ($_POST['itCampoClave'] == 'U') 
+     {           
            $usuario = new Usuario();
            $usuario->setUsuario_id(filter_input(INPUT_POST, 'itUsuario_id',FILTER_SANITIZE_NUMBER_INT));
            $usuario->setNombres(filter_input(INPUT_POST, 'itNombres',FILTER_SANITIZE_STRING));
-           $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));       
+           $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));
+           
+           $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id'); 
+           $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar;
            $ch = curl_init($url);
            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($usuario));
            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-       }
-       elseif ($_POST['itCampoClave'] == 'D') 
-       {
+     }
+     elseif ($_POST['itCampoClave'] == 'D') 
+     {
          $DatoBuscar = filter_input(INPUT_POST, 'itUsuario_id');  
          $url = 'http://localhost:83/TokenForceAPI/usuarios/'.$DatoBuscar ; 
          $ch = curl_init($url);
          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");       
-       }     
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
-          $response = curl_exec($ch);
-          curl_close($ch);  
-          echo $response;
-     
-     }
-      
-
-
- 
+     }     
+        
+     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
+     $response = curl_exec($ch);
+     curl_close($ch);  
+     echo $response;         
