24eba77ec9e39710753dbeb07ae6ca8f038f1649
JCorreal <37590391+JCorreal@users.noreply.github.com>
2019-03-09 09:35:06 -0500

    Update ConsultarTodo.php

--- Consumir_TokenForceAPI/BLL/ConsultarTodo.php
+++ Consumir_TokenForceAPI/BLL/ConsultarTodo.php
@@ -1,11 +1,8 @@
 <?php
 
-
-        $url = 'http://localhost:83/TokenForceAPI/usuarios'; 
-        
+        $url = 'http://localhost:83/TokenForceAPI/usuarios';         
         $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                              
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
         $response = curl_exec($ch);
         curl_close($ch);  
