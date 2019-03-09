03ee2bc40731a73440cc215422d1389c1930159b
JCorreal <37590391+JCorreal@users.noreply.github.com>
2019-03-09 07:56:51 -0500

    Add files via upload

---
+++ Consumir_TokenForceAPI/BLL/ConsultarTodo.php
@@ -0,0 +1,13 @@
+<?php
+
+
+        $url = 'http://localhost:83/TokenForceAPI/usuarios'; 
+        
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
+        $response = curl_exec($ch);
+        curl_close($ch);  
+        echo $response;
+     
