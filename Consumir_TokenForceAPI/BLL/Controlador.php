a96c524d4b23da37e19ec097facf5d7f5c94e15b
JCorreal <37590391+JCorreal@users.noreply.github.com>
2020-02-21 10:10:52 -0500

    Update Controlador.php

--- Consumir_TokenForceAPI/BLL/Controlador.php
+++ Consumir_TokenForceAPI/BLL/Controlador.php
@@ -1,7 +1,7 @@
 <?php
      require_once '../BO/Usuario.php';     
      
-     $url = 'http://localhost:83/TokenForceAPI/usuarios/'. filter_input(INPUT_POST, 'itUsuario_id',FILTER_SANITIZE_NUMBER_INT);
+     $url = 'http://localhost/TokenForceAPI/usuarios/'. filter_input(INPUT_POST, 'itUsuario_id',FILTER_SANITIZE_NUMBER_INT);
      
      if ($_POST['itCampoClave'] == 'R') // R = Read Consultar un Usuario
      {
@@ -20,7 +20,7 @@
         $usuario->setNombres(filter_input(INPUT_POST, 'itNombres',FILTER_SANITIZE_STRING));
         $usuario->setApellidos(filter_input(INPUT_POST, 'itApellidos',FILTER_SANITIZE_STRING));     
                 
-        $ch = curl_init('http://localhost:83/TokenForceAPI/usuarios');
+        $ch = curl_init('http://localhost/TokenForceAPI/usuarios');
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($usuario));        
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");             
@@ -29,4 +29,20 @@
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
      $response = curl_exec($ch);
      curl_close($ch);  
-     echo $response;         
+     
+     if ($_POST['itCampoClave'] == 'R')
+     {
+         $obj = json_decode($response);
+         echo 'Nombres: ';
+         print $obj->{'nombres'}; 
+         echo '<br><br>';
+         echo 'Apellidos: ';
+         print $obj->{'apellidos'}; 
+     }     
+     else
+     {
+         $obj = json_decode($response);
+         print $obj->{'status'}; 
+         echo '<br><br>';
+         print $obj->{'message'};
+     }
