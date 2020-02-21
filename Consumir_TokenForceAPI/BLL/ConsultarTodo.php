c680fcf50841d21788f5b73dd1ba09d9a6a8424a
JCorreal <37590391+JCorreal@users.noreply.github.com>
2020-02-21 10:09:40 -0500

    Update ConsultarTodo.php

--- Consumir_TokenForceAPI/BLL/ConsultarTodo.php
+++ Consumir_TokenForceAPI/BLL/ConsultarTodo.php
@@ -1,10 +1,29 @@
 <?php
 
-        $url = 'http://localhost:83/TokenForceAPI/usuarios';         
+        $url = 'http://localhost/TokenForceAPI/usuarios';         
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                              
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                     
         $response = curl_exec($ch);
         curl_close($ch);  
-        echo $response;
-     
+        
+        $a = json_decode($response, true);
+
+        $s = '<table border="1"> 
+            <tr>                         
+              <th>ID</th>
+              <th>Nombres</th>
+              <th>Apellidos</th>
+            </tr>';
+        
+        foreach ( $a as $r ) {
+                $s .= '<tr>';
+                foreach ( $r as $v ) {
+                        $s .= '<td>'.$v.'</td>';
+                }
+                $s .= '</tr>';
+        }
+        $s .= '</table>';
+
+        echo $s;
+
